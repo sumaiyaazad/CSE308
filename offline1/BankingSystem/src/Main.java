174c650c92d7f06bc27e2a51f0d6429c59b46aa3
sumaiyaazad <azadsumaiya00@gmail.com>
2021-03-05 10:36:31 +0600

    create student using userAccount

--- offline1/BankingSystem/src/Main.java
+++ offline1/BankingSystem/src/Main.java
@@ -1,18 +1,20 @@
 import Accounts.*;
 import Bank.Bank;
+import Employees.Employees;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Bank bank=new Bank();
+        Accounts userAccount;
         Scanner s = new Scanner(System.in);
         for(int i=0;;i++){
             String input=s.nextLine();
             if(input.startsWith("Create")){
                 String[] inputArray=input.split("\\s+");
-                System.out.println("inputArray: "+inputArray[1]);
-                System.out.println("Accounts array: "+bank.getAccounts());
+                //System.out.println("inputArray: "+inputArray[1]);
+                //System.out.println("Accounts array: "+bank.getAccounts());
                 for (Accounts account : bank.getAccounts()) {
                     if(account.getName().equals(inputArray[2])){
                         System.out.println("Sorry! Could not create account(name already taken)");
@@ -20,25 +22,26 @@ public class Main {
                     }
                 }
                 if(inputArray[1].equals("Student")){
-                    Student student=new Student();
-                    student.create(inputArray[2],Integer.parseInt(inputArray[3]));
-                    bank.getAccounts().add(student);
+                    userAccount=new Student();
+                    userAccount.create(inputArray[2],Integer.parseInt(inputArray[3]));
+                    bank.getAccounts().add(userAccount);
                 }
                 if(inputArray[1].equals("Savings")){
-                    Savings savings=new Savings();
-                    savings.create(inputArray[2],Integer.parseInt(inputArray[3]));
-                    bank.getAccounts().add(savings);
+                    userAccount=new Savings();
+                    userAccount.create(inputArray[2],Integer.parseInt(inputArray[3]));
+                    bank.getAccounts().add(userAccount);
                 }
                 if(inputArray[1].equals("FixedDeposit")){
-                    FixedDeposit fixedDeposit=new FixedDeposit();
-                    fixedDeposit.create(inputArray[2],Integer.parseInt(inputArray[3]));
-                    bank.getAccounts().add(fixedDeposit);
+                    userAccount=new FixedDeposit();
+                    userAccount.create(inputArray[2],Integer.parseInt(inputArray[3]));
+                    bank.getAccounts().add(userAccount);
                 }
                 //print Accounts array from bank
                 for(Accounts account : bank.getAccounts()){
                     System.out.println(account.getName()+" "+account.getBalance());
                 }
             }
+
         }
     }
 }
