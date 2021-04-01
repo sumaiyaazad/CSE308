7aada3ba67082dfeb6242d56819471da7be11cf0
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-01 21:38:40 +0600

    pizzadecorator done version 1

---
+++ offline3/Decorator/src/NormalPackageBeef.java
@@ -0,0 +1,2 @@
+public class NormalPackageBeef {
+}
--- offline3/Decorator/src/PizzaDecorator.java
+++ offline3/Decorator/src/PizzaDecorator.java
@@ -1,2 +1,14 @@
-public class PizzaDecorator {
+public class PizzaDecorator implements Pizza{
+    private Pizza newPizza;
+    public PizzaDecorator(Pizza newPizza)  {
+        this.newPizza=newPizza;
+    }
+    @Override
+    public String preparePizza(){
+        return newPizza.preparePizza();
+    }
+    @Override
+    public double pizzaPrice(){
+        return newPizza.pizzaPrice();
+    }
 }
