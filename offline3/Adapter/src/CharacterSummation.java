9467b7b230fa8f2e4e19a65bcdadeb3028b7d333
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-01 19:22:36 +0600

    CharacterSummation done version 1

---
+++ offline3/Adapter/src/CharacterSummation.java
@@ -0,0 +1,11 @@
+public class CharacterSummation implements AdvancedSumCalculator{
+    @Override
+    public void calculateSumInteger(String filename) {
+
+    }
+
+    @Override
+    public void calculateSumCharacter(String filename) {
+        System.out.println("Sum of character(ascii value) ... filename: "+filename);
+    }
+}
