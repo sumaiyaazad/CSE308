445a8d11ce790454f7ccfe1b591d8977e05c9e93
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-01 19:20:41 +0600

    integerSummation done version 1

---
+++ offline3/Adapter/src/IntegerSummation.java
@@ -0,0 +1,11 @@
+public class IntegerSummation implements AdvancedSumCalculator{
+    @Override
+    public void calculateSumInteger(String filename) {
+        System.out.println("Sum of integers ... filename: "+filename);
+    }
+
+    @Override
+    public void calculateSumCharacter(String filename) {
+
+    }
+}
